163166c22beaeb28713aba5ea0fc82c513b5c68a
Mohammed Swalih N K <swalink555@gmail.com>
2026-04-10 09:56:13 +0530

    Time: 0 ms (100%), Space: 13.3 MB (67.24%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        left_sum = 0
+        total_sum = sum(nums)
+        for i,num in enumerate(nums):
+            if left_sum == total_sum - left_sum - num:
+                return i
+            left_sum += num
+        return -1
